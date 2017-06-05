4b8d56bfd00b0f0110ec3bc128f00ac003b64177
Richard Wang <rwang4@stuy.edu>
2017-06-05 14:29:27 -0400

    initial work on djikstra

--- project/static/editor.js
+++ project/static/editor.js
@@ -731,7 +731,17 @@ saveMapBtn.addEventListener("click", saveMap);
 //SAVING AND LOADING
 var saveMap = function(){
     var mapJSON = canvasToJSON();
-    //AJAX
+    $.ajax({
+	    type: "POST",
+	    url : "/saveData/",
+	    dataType: "json",
+	    data: JSON.stringify(mapJSON),
+	    success: function(response) {
+		console.log("works");
+	    error: function(response) {
+		console.log("nope");
+	    }
+	});
     return null;
 }
 
@@ -778,17 +788,6 @@ var canvasToJSON = function(){
 	}
     }
     console.log(canvasJSON);
-    $.ajax({
-	    type: "POST",
-	    url : "/saveData/",
-	    dataType: "json",
-	    data: JSON.stringify(canvasJSON),
-	    success: function(response) {
-		console.log("works");
-	    error: function(response) {
-		console.log("nope");
-	    }
-	});
     return canvasJSON;
 }
 
